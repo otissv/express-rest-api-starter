305b51e27622cd354fe5262030ac9aedc69dabef
otissv <otissv@gmail.com>
2016-04-08 17:52:20 +0200

    fixed responces

--- backend/api/v01/controllers/user-controller-v01.js
+++ backend/api/v01/controllers/user-controller-v01.js
@@ -25,19 +25,24 @@ export default {
     // check header or url parameters or post parameters for token
     User.find({}, (err, users) => {
       if (err) {
-        return res.status(400).send({
-          message: 'No users were found'
+        return res.status(400).json({
+          message: 'Error retriving user'
         });
       }
 
       if (users != null) {
         const userList = users.map(user => userClened(user));
 
-        return res.json(userList);
+        return res.json({
+          success: true,
+          message: 'Users found',
+          result: userList
+        });
 
       } else {
-        return res.status(404).send({
-          message: 'error'
+        return res.status(404).json({
+          sucess: false,
+          message: 'No users were found'
         });
       }
     });
@@ -49,17 +54,23 @@ export default {
 
     User.findById(id, (err, user) => {
       if (err) {
-        return res.status(400).send({
-          message: 'User cannot be found'
+        return res.status(400).json({
+          success: false,
+          message: 'Error retriving user'
         });
       }
 
       if (user != null) {
-        return res.json(userClened(user));
+        return res.json({
+          success: true,
+          message: 'User found',
+          result: userClened(user)
+        });
 
       } else {
-        return res.status(404).send({
-          message: 'error'
+        return res.status(404).json({
+          success: false,
+          message: 'User cannot be found'
         });
       }
     });
@@ -72,16 +83,21 @@ export default {
 
     User.update(id, data, (err, result) => {
       if (err) {
-        return res.status(400).send({
-          message: 'User cannot be found'
+        return res.status(400).json({
+          success: false,
+          message: 'Error updating user'
         });
       }
 
       if (result.nModified === 1) {
-        return res.send({message: 'success'});
+        return res.json({
+          success: true,
+          message: 'User updated'
+        });
       } else {
-        return res.status(404).send({
-          message: 'error'
+        return res.status(404).json({
+          success: false,
+          message: 'User was not updatied'
         });
       }
     });
@@ -93,17 +109,22 @@ export default {
 
     User.remove(id, (err, result) => {
       if (err) {
-        return res.status(400).send({
-          message: 'User cannot be found'
+        return res.status(400).json({
+          success: false,
+          message: 'Error removing user'
         });
       }
 
       if (result.result.n === 1) {
-        return res.status(404).send({
-          message: 'error'
+        return res.status(404).json({
+          success: false,
+          message: 'User was not removed'
         });
       } else {
-        return res.send({message: 'success'});
+        return res.json({
+          success: true,
+          message: 'User removed'
+        });
       }
 
     });
